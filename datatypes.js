541b6fce80320b0302883bed27516b045dcdb1e5
harshit <hrajwani11@gmail.com>
2024-05-29 19:15:42 +0530

    learned conversion of datatypes

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+let score = 33
+
+console.log(typeof score)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let number = 1
+let StringNo = String(number)
+
+console.log(StringNo)
+console.log (typeof StringNo)
