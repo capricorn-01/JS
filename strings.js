b297adb50d78fb6fe727c542a7072898c8fea4b1
harshit <hrajwani11@gmail.com>
2024-06-05 19:27:44 +0530

    learning strings

---
+++ strings.js
@@ -0,0 +1,17 @@
+const name = "Harshit"
+const repocount = 50
+
+// console.log(name + repocount)
+
+console.log(`Hello my name is ${name} My repocount is ${repocount
+}`)
+
+const gamename =  new String ('HarshitOg')
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+console.log(gamename.indexOf("h"))
+console.log(gamename.slice(-7,5))
+console.log(gamename.replace('it','ika'))
+console.log(gamename.includes('Og'))
+console.log(gamename.split(-3))
