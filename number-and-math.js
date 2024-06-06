d7b3c53aa74743cb0b14496c214b91151e9e5464
harshit <hrajwani11@gmail.com>
2024-06-06 19:06:37 +0530

    learning numbers

---
+++ number-and-math.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.8955
+
+console.log(otherNumber.toPrecision(3))
+
+const numbers = 10000000
+console.log(numbers.toLocaleString())
+
+//*******************maths**************** */
+
+
