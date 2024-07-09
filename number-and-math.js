ffd9d2f036a6fcd6f4a33e9ec40caacff5bcbec6
harshit <hrajwani11@gmail.com>
2024-07-09 18:13:33 +0530

    Math

--- number-and-math.js
+++ number-and-math.js
@@ -1,19 +1,31 @@
-const score = 400
-console.log(score)
+// const score = 400
+// console.log(score)
 
-const balance = new Number(100)
-console.log(balance)
+// const balance = new Number(100)
+// console.log(balance)
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
 
-const otherNumber = 123.8955
+// const otherNumber = 123.8955
 
-console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(3))
 
-const numbers = 10000000
-console.log(numbers.toLocaleString())
+// const numbers = 10000000
+// console.log(numbers.toLocaleString())
 
 //*******************maths**************** */
 
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
 
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10 + 1))
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
