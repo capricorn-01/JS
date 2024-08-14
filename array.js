731bbf41f68352364ea465ee1d1b9569f10da459
harshit <hrajwani11@gmail.com>
2024-08-14 15:58:28 +0530

    practice

---
+++ array.js
@@ -0,0 +1,28 @@
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["Batman", "Hulk"]
+
+const myArr2 = new Array (1,2,3,4)
+
+//myArr.unshift(5)
+myArr.shift()
+console.log(myArr);
+
+
+// myArr.push(6)
+// myArr.pop()
+// console.log("A",myArr);
+
+
+
+// const myn1 = myArr.slice(1,3)
+// console.log("B", myArr );
+// console.log(myn1);
+
+// const myn2 = myArr.splice(1,3)
+// console.log("c", myArr );
+
+// console.log(myn2);
+
+
+
+
---
+++ dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+//let myCreatedDate = new Date(2024,0,24)
+let myCreatedDate = new Date("01-01-2002")
+console.log(myCreatedDate.toLocaleDateString());
